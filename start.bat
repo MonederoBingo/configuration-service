8c841da5bf3e0c6c417f8307181c1e1382554b86
alayor <alayor3@gmail.com>
2017-06-07 21:38:20 -0500

    Adding echo message to start script.

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
-@echo off
-call mvn clean install
+echo "::::::::::STARTING CONFIGURATION SERVICE:::::::::::"
+cd %~dp0
+call mvn clean install -DskipTests
 call java -jar target/configuration-0.0.1-SNAPSHOT.jar
